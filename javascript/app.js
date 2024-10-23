a4ed267ede5e0b17a07b4947c9d206fbb6458960
salman imran <shamsisalman81@gmail.com>
2024-10-23 14:15:25 +0500

    include-js

---
+++ javascript/app.js
@@ -0,0 +1,65 @@
+console.log("HELLO - WORLD");
+// decalration initizalization 
+var age; //number
+var name = "salman imran"; //string
+var EID = "21222";
+var phone = "01819110"; //string
+var phone = "01819110"; //string
+var isActive = true ; //boolean
+
+ age  = 32
+
+//  rule
+
+// do
+// symbols that use in var ==> _ $ 
+// if we use number in var if single ===> employee_1
+// var if signle name ==>  name , age , phone etc.. 
+// var if signle name ==>  name , age , phone etc..
+// rules : camel case and higfncase
+// myNameIsSalman
+// my_Name_Is_Salman 
+// donts 
+// id many words ==> nameis , ageis is active
+// if we use number in var if single ===> employee1
+// if we use number in var if single ===> 1employee
+// var if signle name ==>  Name , aGe , pHOne etc.. 
+
+
+// Operators 
+// arthmetic : + - * / %
+// comparison : == >= <= != > <
+// Logical || ==> OR  && ==> AND  ! ==> NOT
+
+var operand_1 = 3
+var operand_2 = 3
+
+console.log("RESULT IS +:", operand_1 + operand_2);
+console.log("RESULT IS -:", operand_1 - operand_2);
+console.log("RESULT IS *:", operand_1 * operand_2);
+console.log("RESULT IS /:", operand_1 / operand_2);
+console.log("RESULT IS %:", operand_1 % operand_2);
+
+console.log("RESULT IS > :",  operand_1 >   operand_2);
+console.log("RESULT IS < :",  operand_1 <   operand_2);
+console.log("RESULT IS >= :", operand_1 >= operand_2 );
+console.log("RESULT IS <= :", operand_1 <= operand_2 );
+console.log("RESULT IS != :", operand_1 != operand_2 );
+console.log("RESULT IS == :", operand_1 == operand_2 );
+
+console.log("RESULT IS || :",   );
+console.log("RESULT IS && :",   );
+console.log("RESULT IS !  :",   );
+
+var val_1 = true;
+var val_2 = true;
+
+if(val_1 && val_2){
+   console.log("salman imran") 
+}else {
+    console.log("sobhan")
+}
+
+console.log(false || false)
+console.log(false && true)
+console.log(!false)
