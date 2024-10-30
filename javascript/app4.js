728cf272224365ad5cc919c242cea933020d1d2b
salman imran <shamsisalman81@gmail.com>
2024-10-30 15:55:21 +0500

    new code

---
+++ javascript/app4.js
@@ -0,0 +1,78 @@
+//display statements
+
+// console.log("hello world");
+// alert("hello s'up ! ");
+
+//reserved keywords..
+//https://www.w3schools.com/js/js_reserved.asp
+
+// case sensitive...
+
+// var Rose = "xyz";
+// console.log(rose);
+
+// shorten the variable
+// bestSupportingActressInADramaOrComedy
+
+// var user = prompt("Enter your name !");
+// var Ques1 = prompt("Enter apple color ?");
+// var ans1 = "red"
+// var ans2 = "pakistan"
+// var ans3 = "islamabad"
+// var score = 0 ;
+// var result = score;
+
+// if(Ques1 === ans1){
+//     result = ++score
+//     alert("your answer is correct");
+// }
+// if(Ques2 === ans2){
+//     result = ++score
+//     alert("your answer is correct");
+// }
+// if(Ques3 === ans3){
+//     result = ++score
+//     alert("your answer is correct");
+// }
+
+// if(Ques1 === ans1){
+//     result = ++score
+//     alert("your answer is correct");
+//     var Ques2 = prompt("Enter where abdullah live ?");
+//     if(Ques2 === ans2){
+//         result = ++score
+//         alert("your answer is correct");
+//          if(Ques3 === ans3){
+//             var Ques3 = prompt("Enter where is shah-faisal-masjid ?");
+//             result = ++score
+//             alert("your answer is correct");
+//         }
+//     }
+// }
+
+// if(score >= 1){
+//     console.log("WIN");
+//     console.log(user + " " +"passing score is" + result);
+// }else{
+//     console.log("Failed");
+//     console.log(user + " " + "passing score is" + result);
+// }
+
+// == && === diffrence
+
+// var num = "1"
+
+//== compare value 
+// === compare value + data type as well
+
+// if(num === 1){
+//    console.log("correct !"); 
+// }else{
+//     console.log("failed !"); 
+// }
+
+// var result = true;
+
+// if(!result){
+//     console.log("hellowolrd")
+// }
